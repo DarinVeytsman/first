8d21b97ba37c857348ee4c116c6d33031af5d629
DarinVeytsman <72801650+DarinVeytsman@users.noreply.github.com>
2020-10-18 11:20:26 +0500

    js123

---
+++ script.js
@@ -0,0 +1,36 @@
+// console.log(1);
+// console.log()
+// console.log('year')
+
+
+
+var name = prompt('Bведите свое имя');
+console.log(' Меня ' + ' зовут ' +  name  );
+
+var age = prompt('Bведите свой возрост ');
+console.log(' Мне ' + age + ' лет ');
+
+var tru = prompt(' Готовы ли вы к опросу? ');
+console.log(" Правда: " + true );
+
+var num = prompt(' Решите пример: 18+5');
+console.log( ' Пример1: ' + '18 + 5 = ' + (18 + 5));
+
+
+var num = prompt(' Решите пример: 23-18');
+console.log(' Пример2: ' + '23 - 18 = ' + (23 - 18));
+
+var num = prompt('Решите пример: 9 * 9');
+console.log(' Пример3: ' + '9 * 9 = '+ (9*9));
+
+var num = prompt(' Решите пример: 6 % 3 ');
+console.log(' Пример4: ' + ' 6 % 3 = ' + (6%3));
+
+var num = prompt(' Решите пример: 99 / 3 ');
+console.log(' Пример5: ' + ' 99 / 3 = ' + (99/3));
+ 
+var str = prompt( ' Решили вы все верно? ');
+console.log(' Ваш ответ: ' + str );
+
+var str = prompt(' Нажимите на "Ok" и переходите в консоль ');
+console.log('Ваш результат:' + ' result ' );
